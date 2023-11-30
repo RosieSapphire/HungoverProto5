ecdf5034262f372225cc91835eda5e9ac29bfdd7
Rosie <jayjaygames40@gmail.com>
2023-11-30 12:30:48 -0600

    Added more detail to the bongometer

--- src/engine/ui.c
+++ src/engine/ui.c
@@ -6,6 +6,7 @@
 
 static rdpq_font_t *ui_font;
 static sprite_t *bongometer_spr;
+static sprite_t *bongometer_arrow_spr;
 static u32 frame_counter;
 
 void ui_font_init(void)
@@ -16,6 +17,7 @@ void ui_font_init(void)
 	});
 	rdpq_text_register_font(1, ui_font);
 	bongometer_spr = sprite_load("rom:/bongometer.ci8.sprite");
+	bongometer_arrow_spr = sprite_load("rom:/bongometer_arrow.ia4.sprite");
 	frame_counter = 0;
 }
 
@@ -41,7 +43,8 @@ void ui_item_qty_draw(const struct item *i, const u8 uses_2nd)
 void ui_bongometer_draw(const struct item *item, const f32 subtick)
 {
 	const f32 usage_timer_lerp =
-		lerpf(item->usage_timer_last, item->usage_timer, subtick);
+		clampf(lerpf(item->usage_timer_last,
+	       item->usage_timer, subtick), 0, 128);
 	const f32 lerpt = clampf(usage_timer_lerp, 0, 8) / 8.0f;
 	const f32 y_pos = smoothf((CONF_HEIGHT >> 1) - 256,
 		   (CONF_HEIGHT >> 1) - 96, lerpt);
@@ -49,4 +52,12 @@ void ui_bongometer_draw(const struct item *item, const f32 subtick)
 	rdpq_set_mode_standard();
 	rdpq_mode_alphacompare(1);
 	rdpq_sprite_blit(bongometer_spr, (CONF_WIDTH >> 1) - 64, y_pos, NULL);
+	rdpq_sprite_blit(bongometer_arrow_spr,
+		  (CONF_WIDTH >> 1), y_pos + 100, &(const rdpq_blitparms_t){
+				    .scale_x = 0.5f,
+				    .scale_y = 0.5f,
+		  		    .cx = 8,
+		  		    .cy = 128,
+		  		    .theta = (-usage_timer_lerp * 0.015f) + 1,
+	});
 }
